2caac1ce74ab7575c8fcf3eb4491492524b75963
sim-hyeongjun <jbb100971@gmail.com>
2022-12-14 17:16:36 +0900

    home->index

--- websaver2/websaver/websaver/urls.py
+++ websaver2/websaver/websaver/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='index'),
     path('parsed_data', include('parsed_data.urls')),
     path('deal', include('deal.urls')),
     path('auction', include('auction.urls')),
